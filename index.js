10282d1751fcdc05955bd25234c848bf3ddedfac
brunobandelli <brunobandelli@gmail.com>
2022-09-03 00:07:02 -0300

    gerando eventos com socket io

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
 io.on("connection",(socket) => {
-    console.log(socket),
-    console.log(socket.id)
+    // console.log(socket),
+    // console.log(socket.id)
+    socket.on("boasvindas", (data) => {
+        console.log("EXECUTANDO EVENTO DE BOAS VINDAS")
+        console.log(data)
+    })
+
+    socket.on("palavra", (data) => {
+        console.log(data);
+        socket.emit("resultado", data + ' - GUIA DO PROGRAMADOR!');
+        //console.log(data);
+    })
 })
 
 app.set("view engine", "ejs")
